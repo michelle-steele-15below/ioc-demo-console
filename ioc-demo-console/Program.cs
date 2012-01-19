c9ea5da1a49adf9d552b4c531c3929481ffd9ef9
michael-steele-15below <michael.steele@15below.com>
2012-01-19 15:00:54 +0000

    Added more depdendencies

--- ioc-demo-console/Program.cs
+++ ioc-demo-console/Program.cs
@@ -12,6 +12,7 @@ namespace ioc_demo_console
         static void Main(string[] args)
         {
             var container = new WindsorContainer();
+            container.Register(Component.For<ISomeOtherThing>().ImplementedBy<SomeOtherThing>());
             container.Register(Component.For<IMyThing>().ImplementedBy<MyThing>());
 
             var myThing = container.Resolve<IMyThing>();
@@ -28,9 +29,29 @@ namespace ioc_demo_console
 
     internal class MyThing : IMyThing
     {
+        private readonly ISomeOtherThing otherThing;
+
+        public MyThing(ISomeOtherThing otherThing)
+        {
+            this.otherThing = otherThing;
+        }
+
         public void DoWork()
         {
-            Console.WriteLine("Doing some work");
+            Console.WriteLine("Doing some work" + otherThing.GetCustomText());
+        }
+    }
+
+    internal interface ISomeOtherThing
+    {
+        string GetCustomText();
+    }
+
+    class SomeOtherThing : ISomeOtherThing
+    {
+        public string GetCustomText()
+        {
+            return " and more work";
         }
     }
 }
