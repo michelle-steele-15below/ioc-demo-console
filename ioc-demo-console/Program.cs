d15e471ab412c4b958bf76ed81a6831a1cb019d8
michael-steele-15below <michael.steele@15below.com>
2012-01-19 14:57:47 +0000

    Added simple Windsor registration

--- ioc-demo-console/Program.cs
+++ ioc-demo-console/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
 
 namespace ioc_demo_console
 {
@@ -9,6 +11,26 @@ namespace ioc_demo_console
     {
         static void Main(string[] args)
         {
+            var container = new WindsorContainer();
+            container.Register(Component.For<IMyThing>().ImplementedBy<MyThing>());
+
+            var myThing = container.Resolve<IMyThing>();
+            myThing.DoWork();
+
+            Console.ReadLine();
+        }
+    }
+
+    internal interface IMyThing
+    {
+        void DoWork();
+    }
+
+    internal class MyThing : IMyThing
+    {
+        public void DoWork()
+        {
+            Console.WriteLine("Doing some work");
         }
     }
 }
